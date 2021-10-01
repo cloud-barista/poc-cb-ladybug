b1cda6fc90f54d11d854eb29f6c8b3aae88340b5
Sooyoung Kim <sykim@etri.re.kr>
2021-10-01 01:55:22 +0000

    test: add mcas rest-api test scripts
    o test scripts
     - get-mcas.sh
       (GET /ladybug/ns/:namespace/mcas)
     - enable-mcas.sh
       (POST /ladybug/ns/:namespace/mcas)
     - disable-mcas.sh
       (DELETE /ladybug/ns/:namespace/mcas)

---
+++ scripts/get-mcas.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# -----------------------------------------------------------------
+# usage
+if [ "$#" -lt 1 ]; then 
+	echo "./get-mcas.sh <namespace>"
+	echo "./get-mcas.sh lb-ns"
+	exit 0; 
+fi
+
+source ./conf.env
+
+# ------------------------------------------------------------------------------
+# const
+
+
+# -----------------------------------------------------------------
+# parameter
+
+# 1. namespace
+if [ "$#" -gt 0 ]; then v_NAMESPACE="$1"; else	v_NAMESPACE="${NAMESPACE}"; fi
+if [ "${v_NAMESPACE}" == "" ]; then 
+	read -e -p "Namespace ? : " v_NAMESPACE
+fi
+if [ "${v_NAMESPACE}" == "" ]; then echo "[ERROR] missing <namespace>"; exit -1; fi
+
+c_URL_LADYBUG_NS="${c_URL_LADYBUG}/ns/${v_NAMESPACE}"
+
+
+# ------------------------------------------------------------------------------
+# print info.
+echo ""
+echo "[INFO]"
+echo "- Namespace                  is '${v_NAMESPACE}'"
+
+
+# ------------------------------------------------------------------------------
+# Get MCAS
+get_mcas() {
+
+	if [ "$LADYBUG_CALL_METHOD" == "REST" ]; then
+
+		curl -sX GET ${c_URL_LADYBUG_NS}/mcas -H "${c_CT}" -H"${c_AUTH}";
+
+	elif [ "$LADYBUG_CALL_METHOD" == "GRPC" ]; then
+
+		#$APP_ROOT/src/grpc-api/cbadm/cbadm cluster create --config $APP_ROOT/src/grpc-api/cbadm/grpc_conf.yaml -i json -o json 
+		echo "[ERROR] not yet implemented"; exit -1;
+
+	else
+		echo "[ERROR] missing LADYBUG_CALL_METHOD"; exit -1;
+	fi
+	
+}
+
+
+# ------------------------------------------------------------------------------
+if [ "$1" != "-h" ]; then 
+	echo ""
+	echo "------------------------------------------------------------------------------"
+	get_mcas;
+fi
