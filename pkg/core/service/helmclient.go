38c3ae25d114b23b207797afa1f4a4ba236b94f1
Sooyoung Kim <sykim@etri.re.kr>
2021-10-26 04:49:27 +0000

    feat: add simple wrapper go-helm-client

---
+++ pkg/core/service/helmclient.go
@@ -0,0 +1,74 @@
+package service
+
+import (
+	"github.com/cloud-barista/cb-mcas/pkg/core/common"
+	hc "github.com/mittwald/go-helm-client"
+	"helm.sh/helm/v3/pkg/repo"
+)
+
+const (
+	defaultHelmCachePath      = "./.go-helm-client/.helmcache"
+	defaultHelmRepoConfigPath = "./.go-helm-client/.helmrepo"
+)
+
+func getHelmClient(namespace string) (hc.Client, error) {
+	// Create global HelmClient for repository
+	opt := &hc.Options{
+		Namespace:        namespace,
+		RepositoryCache:  defaultHelmCachePath,
+		RepositoryConfig: defaultHelmRepoConfigPath,
+		Debug:            true,
+		Linting:          true,
+	}
+
+	hcGeneral, err := hc.New(opt)
+	if err != nil {
+		common.CBLog.Errorf(err.Error())
+		return nil, err
+	}
+
+	return hcGeneral, nil
+}
+
+func getHelmClientFromKubeConf(namespace string, kubeConf *string) (hc.Client, error) {
+	opt := &hc.KubeConfClientOptions{
+		Options: &hc.Options{
+			Namespace:        namespace,
+			RepositoryCache:  defaultHelmCachePath,
+			RepositoryConfig: defaultHelmRepoConfigPath,
+			Debug:            true,
+			Linting:          true,
+		},
+		KubeContext: "",
+		KubeConfig:  []byte(*kubeConf),
+	}
+
+	hcKube, err := hc.NewClientFromKubeConf(opt)
+	if err != nil {
+		common.CBLog.Errorf(err.Error())
+		return nil, err
+	}
+
+	return hcKube, nil
+}
+
+func addPackageRepo(namespace, url string) error {
+	hClient, err := getHelmClient(namespace)
+	if err != nil {
+		return err
+	}
+	//
+	// Add local chartmuseum helm repository
+	//
+	chartRepo := repo.Entry{
+		Name: namespace,
+		URL:  url,
+	}
+
+	// Add a chart-repository to the client
+	if err := hClient.AddOrUpdateChartRepo(chartRepo); err != nil {
+		return err
+	}
+
+	return nil
+}
